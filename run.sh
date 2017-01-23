58a23738a377a139789c435099df632488bf9d1d
Matteo B <matteo@belka.us>
2017-01-23 20:57:00 +0100

    Fix i-1

--- run.sh
+++ run.sh
@@ -41,6 +41,17 @@ if [[ $? -eq 0 ]]; then
     optApp+="${1}"
 
     docker run -d --name translations-tools-postgres --rm -e POSTGRES_PASSWORD=mysecretpassword -d postgres postgres
+
+    prova=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}:5432' translations-tools-postgres)
+    echo $prova
+    inspect_error=${?}
+
+    TIMEOUT=10
+    until docker run -e PGPASSWORD=mysecretpassword --link translations-tools-postgres:pg postgres /usr/bin/psql -h pg -U postgres -d postgres -c "select 1" > /dev/null 2>&1 || [ $TIMEOUT -eq 0 ]; do
+      echo "Waiting for postgres server, $((TIMEOUT--)) remaining attempts..."
+      sleep 1
+    done
+
     docker run -it --rm --name translations-tools -v "$PWD":/usr/src/myapp --link translations-tools-postgres:pg ${optDocker} -w /usr/src/myapp translations-tools python xliffTimeZone.py ${optApp}
 fi
 
